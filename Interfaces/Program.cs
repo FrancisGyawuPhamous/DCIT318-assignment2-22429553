5d58ee426606a521b8b917b7e23817b0ebae25d4
Kwaku Phamous <kwakuphamous21@gmail.com>
2026-09-02 12:48:20 +0000

    Task 3:Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Interfaces
+{
+    // Interface definition
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Class implementing IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Another class implementing IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Task 3: Interfaces ===\n");
+
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
+
